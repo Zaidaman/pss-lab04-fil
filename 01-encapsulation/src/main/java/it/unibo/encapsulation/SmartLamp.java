beb9a02d25a322af13c44d37b4a6f363494d44a8
Filippo Cappella <steve.97ita@gmail.com>
2023-10-17 11:11:13 +0200

    Fix and add comment

--- 01-encapsulation/src/main/java/it/unibo/encapsulation/SmartLamp.java
+++ 01-encapsulation/src/main/java/it/unibo/encapsulation/SmartLamp.java
@@ -2,18 +2,23 @@ package it.unibo.encapsulation;
 
 public class SmartLamp {
 
-    private boolean light = false;
-    private int intensity = 0;
+    //color palette inizialization as a private static array
+    private final static String[] COLOR_PALETTE = ["white", "red", "blue", "green"];
+
+    //inizialization of variables
+    private boolean lightIsOn = false;
+    private int intensity = 1;
     private String color = "white";
 
     public SmartLamp(boolean light, int intensity, String color){
-        this.light = light;
+        this.lightIsOn = lightIsOn;
         this.intensity = intensity;
         this.color = color;
     }
 
+    //function that retuns various status of the of the lamp
     public boolean getLight(){
-        return this.light;
+        return this.lightIsOn;
     }
 
     public int getIntensity(){
@@ -24,37 +29,37 @@ public class SmartLamp {
         return this.color;
     }
 
+    //function to switch on or off the lamp, based on his current status
     public void switchLight(){
-        this.light = !this.light;
+        this.lightIsOn = !this.lightIsOn;
     }
 
+    //switch intensity of the light by taking in input a number from 1 to 3
+    //if the light is off(false) nothing will change, if the number is out of
+    //the range will put the closer intensity
     public void switchIntensity(int n){
-        if light == false{
-            System.Out.println("The light is off");
-            break;
-        }
-
-        if n >= 1 && n <= 3{
-            this.intensity = n;
-        }
-        else{
-            this.intensity = 1;
+        if (this.lightIsOn == true){
+            if (n >= 1 && n <= 3){
+                this.intensity = n;
+            } 
+            else if (n > 3){
+                this.intensity = 3;
+            } else {
+                this.intensity = 1;
+            }
         }
     }
 
+    //switch color based on the color palette and the number n, if the light is
+    //off(false) nothing will change, if n is in the range of the color palette
+    //lenght the new color will be assigned, otherwise it will assign white
     public void swithColor(int n){
-        if light == false{
-            System.Out.println("The light is off");
-            break;
-        }
-
-        private final String[] colorPalette = ["white","red","blue","green"];
-
-        if n <= colorPalette.lenght && n > 0{
-            this.color = colorPalette[n];
-        }
-        else{
+        if (this.lightIsOn == true){
+            if (n <= COLOR_PALETTE.lenght && n > 0){
+                this.color = COLOR_PALETTE[n];
+            } else {
             this.color = "white";
+            }
         }
     }
 
